944d850e212a4cb100b8ab15351e318c67e00f49
Chibuike <malachiokwor@gmail.com>
2022-09-13 15:35:08 -0700

    next

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,13 @@
 #include "main.h"
 /**
-* print_alphabet(void)
+ * print_alphabet - functions to print abc
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
 {
 	char c;
-	
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
