127c8be13c113a1757dcecca398393f344fa40f4
Chibuike <malachiokwor@gmail.com>
2022-09-13 06:44:15 -0700

    coded

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,14 @@
-#!/bin/bash
-gcc int puts(const char* Programming is like building a multilingual puzzle);
+#include <stdio.h>
 
+/**
+* main - print the string in the put function 
+*
+* Description: using the main function 
+*this program prints "programming is like building a multilingual puzzle 
+* Return: 0
+*/
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
